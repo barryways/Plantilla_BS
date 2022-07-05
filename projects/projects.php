67b399f543201263fcf04de2d35bf390fe9b2374
andrespa612 <38359603+andrespa612@users.noreply.github.com>
2022-07-04 20:36:01 -0600

    imgs y carousel

--- projects/projects.php
+++ projects/projects.php
@@ -1,3 +1,39 @@
 <div class="projects">
-    
+<div class="row proy-card">
+        <div id="carouselExampleIndicators" class="carousel slide col-4 ms-2" data-bs-ride="true">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                <img src="<?php echo $url;?>src/img/example.jpg" class="d-block w-100">
+                </div>
+                <div class="carousel-item">
+                <img src="<?php echo $url;?>src/img/example2.jpg" class="d-block w-100">
+                </div>
+                <div class="carousel-item">
+                <img src="<?php echo $url;?>src/img/example.jpg" class="d-block w-100">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+        <div class="col-7">
+            <h2 class="proy-title row">Titulo del proyecto</h2>
+            <p class="proy-text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta a praesentium, tenetur quae minus, beatae, culpa quia esse assumenda soluta libero ipsam natus inventore? Soluta maxime amet voluptatum assumenda reprehenderit?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, velit officiis vitae explicabo at deleniti sed minus ex dolor delectus nihil quas officia vero odio repudiandae facilis, esse, consequuntur sit.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima at inventore, ullam laborum provident veritatis ut nulla eligendi deserunt earum eveniet perspiciatis nisi excepturi quaerat omnis, eos cupiditate esse iste.
+            </p>
+            <a href="#" class="proy-link" target="_blank">https://www.example.com/</a>
+        </div>
+    </div>
 </div>
