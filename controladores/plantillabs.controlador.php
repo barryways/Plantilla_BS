64522cd82dca49a7aaa2b9ba61426644a72041dc
andrespa612 <38359603+andrespa612@users.noreply.github.com>
2022-07-12 23:02:35 -0600

    cards dinamicas

--- controladores/plantillabs.controlador.php
+++ controladores/plantillabs.controlador.php
@@ -4,4 +4,10 @@ class controladorBS{
     public function plantilla(){
 		include "plantilla.php";
 	}
+	static public function mostrarProyectos($item,$valor)
+	{
+		$tabla = "proyectos";
+		$respuesta = modeloPlantillaBS::modeloPlantillaBSDatos($tabla, $item, $valor);
+		return	$respuesta;
+	}
 }
