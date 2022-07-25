cb305c44776048f78e15ed147ef97e886203510b
barryways <93161779+barryways@users.noreply.github.com>
2022-07-25 14:55:35 -0600

    Push de Carlos 25/07 para mostrara a Marck, corregir navbar

---
+++ navbar/navbar.php
@@ -0,0 +1,33 @@
+
+<div class="header">
+    <div class="barra">
+        
+    <nav class="navbar">
+        <a class="navbar__campo text-decoration-none" href="#">About Me</a>
+        <a class="navbar__campo text-decoration-none" href="#">Contact</a>
+        <a class="navbar__campo text-decoration-none" href="#">Projects</a>
+    </nav>
+    </div>
+    <div class="conjunto-logo">
+            <a href="navbar.php">
+            <img  class="logo text-decoration-none" src="<?php echo $url;?>src/img/logo-provicional.png" alt="logo Break Studios">
+            </a> 
+        <h1 class="logo-text">Break <br>Studios</h1>
+    </div>
+    <div class="frase-navbar">
+    <h3 class="frase">
+    <span>You </span>can experiment <span>more </span>than a <span>experience</span>...
+    </h3>
+    </div>
+    <div class="info-navbar">
+        <div class="cuadro-navbar">
+
+        </div>
+        <div class="cuadro-navbar">
+
+        </div>
+        <div class="cuadro-navbar">
+
+        </div>
+    </div>
+</div>
